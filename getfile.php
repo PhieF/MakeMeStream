1e6fc5a9b7d907206bb12a529c0d803e5606f76c
PhieF <phie@phie.ovh>
2018-10-07 01:00:44 +0200

    Delete getfile.php

--- getfile.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if(!empty($_GET['url'])){
-        $url = $_GET['url'];
-        if(strpos($url, 'videos/embed') !== false && strpos($url, 'https://')===0){
-            $file = file_get_contents($url);
-        }
-    }
-
-?>
